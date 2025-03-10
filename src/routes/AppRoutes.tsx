07ac9c1340da58aa45ad5c3c6c07b38841f9ad84
BaoManh210 <manh_2151220070@dau.edu.vn>
2025-03-10 19:06:35 +0700

    update-fix

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,11 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { Login } from "../pages/Auth/Login";
 import { Register } from "../pages/Auth/Register";
-<<<<<<< HEAD
-import MainLayOut from "../cpn/MainLayOut";
-import ShipmentTracking from "../components/UI/Shipment";
-import Order from "../components/UI/Order";
-=======
+
 import MainLayOut from "../LayOut/MainLayOut";
 import Profile from "../pages/Home/Profile";
 import Manage from "../pages/Home/Manage";
@@ -14,19 +10,12 @@ import Revenue from "../pages/Home/Revenue";
 import Shipment from "../pages/Home/Shipment";
 import OrderProblem from "../pages/Home/OrderProblem";
 import Order from "../pages/Home/Order/Order";
->>>>>>> 3d03d06a2bee11c28846e24520052785ec794686
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayOut />}>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="Shipment" element={<ShipmentTracking />} />
-        <Route path="Order" element={<Order />} />
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
@@ -35,7 +24,6 @@ export const AppRoutes = () => {
         <Route path="/shipment" element={<Shipment />} />
         <Route path="/orderProblem" element={<OrderProblem />} />
         <Route path="/order" element={<Order />} />
->>>>>>> 3d03d06a2bee11c28846e24520052785ec794686
       </Route>
     </Routes>
   );
